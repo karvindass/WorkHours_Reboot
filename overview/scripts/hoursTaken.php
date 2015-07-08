bf90c403a4245826d92ae68a6bc4833924717b4e
karvindass <karvin9@gmail.com>
2015-07-08 12:01:39 +0700

    Script to calculate hours used up on a day
    Function made, recalls hours taken up on that day by searching database
    and using query

---
+++ overview/scripts/hoursTaken.php
@@ -0,0 +1,22 @@
+<?php
+
+function callHours($callDate) {
+    include '../Dashboard/scripts/connectdb.php';
+    $query = "SELECT SUM(whperday) "
+            . "AS daySum "
+            . "FROM projects "
+            . "WHERE startdate <= '" . $callDate
+            . "' AND enddate >= '" . $callDate . "'";
+
+    $result = mysqli_query($link, $query);
+    if (!$result) {
+        $error = 'Error fetching projects:' . mysqli_error($link);
+        echo $error;
+    }
+    $dayWHvar = mysqli_fetch_array($result);
+    $daySum = $dayWHvar['daySum'];
+
+    return $daySum;
+}
+
+?>
