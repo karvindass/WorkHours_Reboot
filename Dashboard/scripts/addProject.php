48393d3923e60f740aa4b91700bd40ef97e8b6ae
karvindass <karvin9@gmail.com>
2015-07-09 15:08:05 +0700

    Add Project - Error and Success message
    Error and success alerts placed under form-well

--- Dashboard/scripts/addProject.php
+++ Dashboard/scripts/addProject.php
@@ -24,14 +24,20 @@ if (isset($_POST['submit'])) {
             . $clientID . "', '" . $sumWH . "', '" . $dayWH . "', '" . $payOrder . "', '" . $comments . "')";
 
     if (!mysqli_query($link, $posting)) {
-        echo 'Oops, something went wrong :o, check what you entered';
-        exit();
-    }
-    else {
-        echo '<div class="col-xs-6">
+        $notepop = '<div class="col-xs-6">'
+                . '<div class="alert alert-dismissible alert-danger">'
+                . '<button type="button" class="close" data-dismiss="alert">×</button>'
+                . '<strong>Oh snap!</strong><br><a href="#" class="alert-link">Make sure you put the right things in the inputs</a>'
+                . ' and try submitting again.'
+                . '<br>Please avoid the use of quotation marks of any sort, forward slashes, back slashes and any other symbols'
+                . '<br>This issue should be fixed soon by the web admins'
+                . '</div>'
+                . '</div>';
+    } else {
+        $notepop = '<div class="col-xs-6">
             <div class="alert alert-dismissible alert-success">
             <button type="button" class="close" data-dismiss="alert">×</button>
-            <strong>' . $projectTitle . '</strong> has been successfully added.
+            <strong>' . $projectTitle . '</strong><br>has been successfully added.
             </div>
             </div>';
     }
