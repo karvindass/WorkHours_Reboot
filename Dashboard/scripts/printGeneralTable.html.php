660eb152093af77e6e4875b60f1304119ced42df
karvindass <karvin9@gmail.com>
2015-07-09 10:29:06 +0700

    Table View prints more info
    Added startdate, work hours per day, enddate and changed order

--- Dashboard/scripts/printGeneralTable.html.php
+++ Dashboard/scripts/printGeneralTable.html.php
@@ -1,9 +1,12 @@
 <?php
-    foreach ($projects as $project)
-{
+
+foreach ($projects as $project) {
     echo "<tr>";
     echo '<td>' . $project['title'] . '</td>';
     echo '<td>' . $project['client'] . '</td>';
+    echo '<td>' . $project['startdate'] . '</td>';
+    echo '<td>' . $project['whperday'] . '</td>';
+    echo '<td>' . $project['enddate'] . '</td>';
     echo '<td>' . $project['sumwh'] . '</td>';
     echo "</tr>";
 }
