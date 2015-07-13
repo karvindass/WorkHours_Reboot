1bd7269c1d7aad8a8dc91c59e3da13b793892c55
karvindass <karvin9@gmail.com>
2015-07-13 14:42:36 +0700

    Login - initial scripts
    Verifies login information after logging in, then assigns username,
    firstname and lastname to $_SESSION

---
+++ login/scripts/login.php
@@ -0,0 +1,42 @@
+<?php
+
+session_start();
+
+$error = '';
+
+if (isset($_POST['submit'])) {
+    if (empty($_POST['username']) || empty($_POST['password'])) {
+        $error = 'Username or Password is invalid';
+    } else {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        include 'connectdb.php';
+
+        $username = stripslashes($username);
+        $password = stripslashes($password);
+        $username = mysql_real_escape_string($username);
+        $password = mysql_real_escape_string($password);
+
+        $query = "SELECT * FROM accounts WHERE username='" . $username
+                . "' AND password='" . $password . "'";
+        $result = mysqli_query($link, $query);
+        // check if result exists
+        $rows = mysqli_num_rows($result);
+
+        if ($rows == 1) {
+            $_SESSION['login_user'] = $username;
+            echo $_SESSION['login_user'];
+            while ($row = mysqli_fetch_array($result)) {
+                $_SESSION['login_firstname'] = $row['firstname'];
+                echo $_SESSION['login)firstname'];
+                $_SESSION['login_lastname'] = $row['lastname'];
+            }
+             header("Location: ../../Dashboard");
+        } else {
+            $error = 'Username or Password is invalid';
+        }
+    }
+}
+echo $error;
+?>
