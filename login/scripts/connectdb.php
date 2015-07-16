5cd0a7db08cde60c84051b54e9661aee4a41cb4f
karvindass <karvin9@gmail.com>
2015-07-16 17:00:44 +0700

    connectdb - point locally hosted at HostGator site

--- login/scripts/connectdb.php
+++ login/scripts/connectdb.php
@@ -1,18 +1,18 @@
 <?php
 
-$link = mysqli_connect('localhost', 'root', '');
+$link = mysqli_connect('198.57.247.206:3306', 'karvinda_user', 'Shyf1FBvWU4q');
 if (!$link) {
     $output = 'Unable to connect to the database server.';
-    echo $output;
+//    echo $output;
     exit();
 }
-if (!mysqli_select_db($link, 'workhours1')) {
-    $output = 'Could not find workhours database.';
-    echo $output;
+if (!mysqli_select_db($link, 'karvinda_workhours1')) {
+    $output = 'Could not find projct database.';
+//    echo $output;
     exit();
 }
 
 $output = 'Database connection established.';
 
-// echo $output;
+ echo $output;
 ?>
